896eb47a2a851d5187018823c70bce78a4d4b978
hiradary <hiradwork@gmail.com>
2019-10-09 08:50:17 +0330

    Fix getting started text

--- website/src/screens/home/components/FeaturesSection/FeaturesSection.js
+++ website/src/screens/home/components/FeaturesSection/FeaturesSection.js
@@ -1,11 +1,11 @@
-import React from "react"
+import React from 'react';
 
 //local
-import "./FeaturesSection.css"
-import { GuideCard } from "../../../../components"
-import { FEATURES } from "../../../../constants"
+import './FeaturesSection.css';
+import {GuideCard} from '../../../../components';
+import {FEATURES} from '../../../../constants';
 
-const Feature = ({ title, description, code, file }) => {
+const Feature = ({title, description, code, file}) => {
   return (
     <article className="Feature">
       <GuideCard title={title} description={description} code={code} />
@@ -13,46 +13,42 @@ const Feature = ({ title, description, code, file }) => {
         <img src={file} alt={title} />
       </div>
     </article>
-  )
-}
+  );
+};
 
 const GettingStarted = () => {
   return (
     <article className="GettingStarted">
       <h2 className="-title">Getting started</h2>
-      <p className="-description">
-        Welcome to react-calendar-modern-datepicker!
-      </p>
+      <p className="-description">Welcome to react-native-calendar-modern-datepicker!</p>
       <ol>
         <li>
           <p className="-description">
-            1- The package is both <strong>Android</strong> and{" "}
-            <strong>iOS</strong> compatible.
+            1- The package is both <strong>Android</strong> and <strong>iOS</strong> compatible.
           </p>
         </li>
         <li>
           <p className="-description">
-            2- This package uses <strong>React hooks</strong>. Make sure you're
-            running react-native >= 0.59.0 version.
+            2- This package uses <strong>React hooks</strong>. Make sure you're running react-native
+            >= 0.59.0 version.
           </p>
         </li>
         <li>
           <p className="-description">
-            3- By default, this package inherits font-family from your project
-            ('System'). You can use custom font, See <strong>options</strong>{" "}
-            props.
+            3- By default, this package inherits font-family from your project ('System'). You can
+            use custom font, See <strong>options</strong> props.
           </p>
         </li>
         <li>
           <p className="-description">
-            4- You might see performance issues in development mode. However,
-            you won't see such thing in <strong>production build</strong>.
+            4- You might see performance issues in development mode. However, you won't see such
+            thing in <strong>production build</strong>.
           </p>
         </li>
       </ol>
     </article>
-  )
-}
+  );
+};
 
 const FeaturesSection = () => {
   return (
@@ -64,7 +60,7 @@ const FeaturesSection = () => {
         ))}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export { FeaturesSection }
+export {FeaturesSection};
